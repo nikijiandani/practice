1620b649a24fdfc8703fe94ca4b0424210a80cd4
Niki Jiandani <niki.jiandani@gmail.com>
2019-05-22 21:20:24 +0000

    Merge Arrays assignment

--- playground.js
+++ playground.js
@@ -1,32 +1,8 @@
-function decodeMessage(message){
-    let arr = message.split('');
-    console.log(arr);
-    for(var i = 0; i < arr.length; i++){
-      var letter = arr[i];
-      console.log(letter);
-      switch(letter){
-        case '4': 
-          arr[i] = "a";
-          break;
-        case '3':
-          arr[i] = "e";
-          break;
-        case '1':
-          arr[i] = "i";
-          break;
-        case '0':
-          arr[i] = "o";
-          break;
-        case '2':
-          arr[i] = "u";
-          break;
-        case '5':
-          arr[i] = "y";
-          break;
-      }
-    }
-     arr[73] = 'l';
-     console.log(arr.join(""));
-    return arr.join("");
-  }
-  console.log(decodeMessage("th1s 1s 4 t3st. th1s 1s 0nl5 4 t3st. 1f th1s w3r3 4 r34l m3ss4g3, 502 w021d g3t s0m3th1ng m34n1ngf2l."));
+function merge(arr1, arr2){
+    let result = arr1.concat(arr2).sort((a, b) => a - b);
+    return result;
+}
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
